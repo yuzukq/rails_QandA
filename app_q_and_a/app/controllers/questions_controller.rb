0e64d03de08ecced17c44a810dacffbc6268dd76
VR-Yuzu <109357821+yuzukq@users.noreply.github.com>
2025-03-28 13:21:53 +0000

    firstCommit

---
+++ app_q_and_a/app/controllers/questions_controller.rb
@@ -0,0 +1,31 @@
+class QuestionsController < ApplicationController
+  # 質問一覧を表示
+  def index
+  
+  end
+
+  # 質問ページの詳細表示
+  def show
+
+  end
+
+  # 質問の作成
+  def new
+  end
+
+  # 質問登録
+  def create
+  end
+
+  # 質問編集
+  def edit
+  end
+
+  # 質問更新
+  def update
+  end
+
+  # 質問の削除
+  def detroy
+
+end
