8394a88aa3c7d8ac9bd8ddc6512636e0386a5df6
VR-Yuzu <109357821+yuzukq@users.noreply.github.com>
2025-03-29 05:07:51 +0000

    一覧表示のビューを作成,新規投稿のコントローラの作成

--- app_q_and_a/app/controllers/questions_controller.rb
+++ app_q_and_a/app/controllers/questions_controller.rb
@@ -1,12 +1,14 @@
 class QuestionsController < ApplicationController
   # 質問一覧を表示
   def index
-  
+    @questions = Question.all
+    
   end
 
   # 質問ページの詳細表示
   def show
-
+    @question = Question.find(params[:id]) # @questionにDBのパラメータを格納　-> viewで表示
+    
   end
 
   # 質問の作成メソッド
@@ -16,7 +18,13 @@ class QuestionsController < ApplicationController
   end
 
   # 質問登録
-  def create
+  def create # submitされたときに呼び出される
+    # Questionモデルの初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBに保存
+    @question.save
+    # showアクションへリダイレクト
+    redirect_to @question
   end
 
   # 質問編集
@@ -30,5 +38,11 @@ class QuestionsController < ApplicationController
   # 質問の削除
   def detroy
   end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :name, :content) # ストロングパラメータ
+  end
+
 
 end
